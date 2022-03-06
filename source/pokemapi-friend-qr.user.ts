e4974a96cb05653a1b9c4bf3018ec6a3bd1b33cf
wiinuk <wii.nukegara@gmail.com>
2022-03-06 12:53:24 +0900

    `@updateURL` を削除

--- source/pokemapi-friend-qr.user.ts
+++ source/pokemapi-friend-qr.user.ts
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         Pokemapi Friend QR
 // @namespace    https://github.com/wiinuk/pokemapi-friend-qr
-// @updateURL    https://github.com/wiinuk/pokemapi-friend-qr/raw/master/pokemapi-friend-qr.user.js
 // @homepageURL  https://github.com/wiinuk/pokemapi-friend-qr
 // @version      0.1
 // @description  Add QR code to friend list
